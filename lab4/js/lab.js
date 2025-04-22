9eb89256f48eb17960ad3c7c3ba4f828590e4a36
serikyu <lbuendia@ucsc.edu>
2025-04-21 20:04:46 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,11 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Cookie Clicker
+// An implementation of the game
+// Author: Ren Buendia (lbuendia@ucsc.edu)
+// Date: 4/21/2025
+
+// Display image of cookie
+// Display cookie counter
+// When user clicks cookie:
+// Make the cookie shrink, then enlarge back to original size
+// Increase Cookie Counter by 1
+// When user makes an upgrade purchase:
+// Subtract price of upgrade from Cookie Counter
+// Increase multiplier of cookie clicker
+// Earn achievements
+
+
+
 
 // Constants
 
 // Functions
-
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
