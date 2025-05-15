cc4104e11f5b04557f863b440ff1e04405c5c265
serikyu <lbuendia@ucsc.edu>
2025-05-15 08:51:12 -0700

    lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,26 @@
+// index.js - Libraries and jQuery
+// Requirements: jQuery must be loaded for this script to work.
+// Author: Ren Buendia
+// Date: 5/15/2025
+
+
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    const min = 3
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+}
+
+$(document).ready(function() {
+    // click listener for button
+    $("#make-convo").click(function(){
+      // get new fake dialogue
+      const newText = generateRandomText();
+      // append a new div to our output div
+      $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+    });
+  });
