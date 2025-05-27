d005a685abe1efccc762310cf806c881eb7a2a7f
serikyu <lbuendia@ucsc.edu>
2025-05-26 18:05:17 -0700

    lab13

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -7,14 +7,14 @@ function sortingHat(str) {
   let mod = length % 4;
 
   if (mod === 0) {
-    return "Gryffindor: you're someone who defends the underdog, questions authority, often leaps before looking, is known for joking around, and treats board games like serious business";
-} else if (mod === 1) {
-    return "Ravenclaw: you're a person with strong values, a dedicated worker, an incredibly loyal friend, someone who believes participation matters most, and you always bring the best snacks";
-} else if (mod === 2) {
-    return "Hufflepuff: you're analytical, a high achiever, a bit dreamy or whimsical, confidently unique, and full of fascinating trivia";
-} else {
-    return "Slytherin: you're probably driven and strategic, with the potential to do great things. You're often ahead of the curve, have a sharp wit, care about your image, dress with intention, and keep your softer side hidden";
-}
+      return "Gryffindor: you're the type of person who likes to stand up for the little guy, challenges authority, has a tendency to act first and think later, is known as a class clown and takes board games very seriously";
+  } else if (mod === 1) {
+      return "Ravenclaw: you're the type of person who has a strong moral compass, always works hard, is the most loyal friend, knows it is the taking part that counts and always has the best snacks";
+  } else if (mod === 2) {
+      return "Hufflepuff: you're the type of person who analyses everything, is an overachiever, can be described as away with the fairies, is not afraid to be an individual and has a head stuffed full of interesting facts";
+  } else {
+      return "Slytherin: you are most likely ambitious, shrewd and possibly destined for greatness. We can imagine you're the kind of person who is always one step ahead, has a dark sense of humour, thinks reputation is important, takes pride in their appearance and doesn't let anyone see their soft side";
+  }
 }
 
 $("#button").click(function () {
